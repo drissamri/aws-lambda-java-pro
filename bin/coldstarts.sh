698dc7d858ef1e6e8b70593dc678bebdbf111664
Driss Amri <driss.amri@gmail.com>
2021-08-31 09:43:11 +0200

    enable AWS Distro for OTEL

--- bin/coldstarts.sh
+++ bin/coldstarts.sh
@@ -1,1 +1,1 @@
-lumigo-cli measure-lambda-cold-starts -n favorites-service-dev-AddFavoriteLambda-6egYmMb9d7VN -r eu-west-1 -f src/main/test/resources/event.json
+lumigo-cli measure-lambda-cold-starts -n favorites-service-dev-AddFavoriteLambda-GIL03fbCXfXA -r eu-west-1 -f src/main/test/resources/event.json
